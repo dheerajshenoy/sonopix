3aeb00654dfaa40393dadad3b60c616f25b6fdde
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-05-20 23:37:45 +0200

    Fix script.lua

--- script.lua
+++ script.lua
@@ -1,23 +1,12 @@
 local s = sonopix
 
-
-sonopix.opts.sonify_func = function(ctx)
-    local freq = ctx.fmin + ctx.brightness * (ctx.fmax - ctx.fmin)
-    -- ctx fields: brightness, x, y, width, height, fmin, fmax, scale,
-    --             sample_rate, sample_index, frame_index
-    return ctx.brightness * math.cos(2 * math.pi * freq * ctx.sample_index / ctx.sample_rate)
-end
-
-
 local opened = s.open_file("/home/neo/Downloads/abstract-whale-png.png")
 if opened then
     s.opts = {
-
-        direction = "circle-outwards",
-        -- direction = "top-to-bottom",
+        direction = "top-to-bottom",
         frequency = {
-            min = 20,
-            max = 20000,
+            min   = 20,
+            max   = 20000,
             scale = "log",
         },
         cursor = {
@@ -25,9 +14,14 @@ if opened then
             color = "#FF5000FF",
         },
         spu = 1e-3,
-        amplitude = 0.5,
     }
-    -- s.opts.direction = "top-to-bottom"
+
+    local phase = 0.0
+    s.opts.sonify_func = function(ctx)
+        local freq = ctx.fmin * (ctx.fmax / ctx.fmin) ^ ctx.brightness
+        phase = phase + 2 * math.pi * freq / ctx.sample_rate
+        return ctx.brightness * math.sin(phase)
+    end
 
     if s.sonify() then
         s.play()
